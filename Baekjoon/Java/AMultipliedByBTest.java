bb6efff40b9ea39feab440f1d25419bce0a7c603
dihoon <dihoon955@gmail.com>
2022-08-23 03:15:16 +0900

    AxB

---
+++ Baekjoon/Java/AMultipliedByBTest.java
@@ -0,0 +1,17 @@
+// AxB
+// https://www.acmicpc.net/problem/10998
+
+import java.util.Scanner;
+
+public class AMultipliedByBTest {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		System.out.println(a*b);
+		input.close();
+	}
+}
