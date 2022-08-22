16d44026155671c0871cb7dae7c81c41f7eeff25
dihoon <dihoon955@gmail.com>
2022-08-22 18:59:06 +0900

    A+B

---
+++ Baekjoon/Java/AplusBTest.java
@@ -0,0 +1,18 @@
+// A+B
+// https://www.acmicpc.net/problem/1000
+
+import java.util.Scanner;
+
+public class AplusBTest {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		System.out.println(a + b);
+		
+		input.close();		
+	}
+}
