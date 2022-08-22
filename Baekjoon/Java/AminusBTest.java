385ed39d537281d71fe55e0a223b7af520d0c727
dihoon <dihoon955@gmail.com>
2022-08-22 19:00:31 +0900

    A-B

---
+++ Baekjoon/Java/AminusBTest.java
@@ -0,0 +1,16 @@
+// A-B
+// https://www.acmicpc.net/problem/1001
+import java.util.Scanner;
+
+public class AminusBTest {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		System.out.println(a-b);
+		input.close();
+	}
+}
