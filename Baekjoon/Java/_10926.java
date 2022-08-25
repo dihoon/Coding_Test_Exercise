3dfc1d505e704c3871a54a026f555cf7fc60d3dc
dihoon <dihoon955@gmail.com>
2022-08-26 05:16:22 +0900

    ??!

---
+++ Baekjoon/Java/_10926.java
@@ -0,0 +1,15 @@
+// ??!
+// https://www.acmicpc.net/problem/10926
+import java.io.*;
+
+public class _10926 {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		
+		System.out.println(line+"??!");
+		
+		br.close();
+	}
+}
