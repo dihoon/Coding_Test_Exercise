96da2b2e11ef80e33d7fd0d7d9ed322771273fea
dihoon <dihoon955@gmail.com>
2022-08-31 08:20:14 +0900

    고양이

---
+++ Baekjoon/Java/_10171.java
@@ -0,0 +1,12 @@
+// 고양이
+// https://www.acmicpc.net/problem/10171
+
+public class _10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
