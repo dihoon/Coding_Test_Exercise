dc8a6f2a0bfad39326bcce35b541c45edbd7a874
dihoon <dihoon955@gmail.com>
2022-09-02 03:30:07 +0900

    새싹

---
+++ Baekjoon/Java/_25083.java
@@ -0,0 +1,14 @@
+// 새싹
+// https://www.acmicpc.net/problem/25083
+
+public class _25083 {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
