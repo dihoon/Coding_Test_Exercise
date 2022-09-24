c677f82194254994ff7eee496217416d2436f0b2
dihoon <dihoon955@gmail.com>
2022-09-25 01:10:01 +0900

    짝수와 홀수

---
+++ Javascript/Programmers/_12937.js
@@ -0,0 +1,10 @@
+// 짝수와 홀수 
+// https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+function solution(num) {
+    if (num % 2=== 0){
+        return "Even";
+    }else{
+        return "Odd";
+    }
+}
