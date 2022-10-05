eb304f3eaf3ee939eb714d31f063e595af769eae
dihoon <dihoon955@gmail.com>
2022-10-06 00:19:14 +0900

    콜라츠 추측

---
+++ Javascript/Programmers/_12943.js
@@ -0,0 +1,13 @@
+// 콜라츠 추측
+// https://school.programmers.co.kr/learn/courses/30/lessons/12943
+
+function solution(num) {
+    let cnt = 0;
+    while (num !== 1){
+        if (cnt === 500) return -1;
+        if(num % 2 === 0) num /= 2;
+        else num = num*3 + 1;
+        cnt++;
+    }
+    return cnt;
+}
