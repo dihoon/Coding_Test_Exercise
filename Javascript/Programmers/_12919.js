96811954468e470aac7120ca8473a83394f9ad9b
dihoon <dihoon955@gmail.com>
2022-10-06 09:55:29 +0900

    서울에서 김서방 찾기

--- Javascript/Programmers/_12919.js
+++ Javascript/Programmers/_12919.js
@@ -1,10 +1,6 @@
 // 서울에서 김서방 찾기
 // https://school.programmers.co.kr/learn/courses/30/lessons/12919
 
-const seoul = ["Jane", "Kim"];
-
 function solution(seoul) {
     return `김서방은 ${seoul.indexOf("Kim")}에 있다`; 
 }
-
-console.log(solution(seoul));
