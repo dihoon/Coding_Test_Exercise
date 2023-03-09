6e6c7e5fca880d2761ff7c987f0140c0cc306022
dihoon <dihoon955@gmail.com>
2023-03-10 00:55:18 +0900

    숫자 찾기

---
+++ Javascript/Programmers/120904.js
@@ -0,0 +1,7 @@
+// 숫자 찾기
+// https://school.programmers.co.kr/learn/courses/30/lessons/120904
+
+function solution(num, k) {
+  const result = [...num.toString()].indexOf(`${k}`);
+  return result === -1 ? -1 : result + 1;
+}
