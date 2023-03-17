cc4426d45b9dee65f45d78da8b704e50c4285a70
dihoon <dihoon955@gmail.com>
2023-03-17 16:29:35 +0900

    영어가 싫어요

---
+++ Javascript/Programmers/120894.js
@@ -0,0 +1,22 @@
+// 영어가 싫어요
+// https://school.programmers.co.kr/learn/courses/30/lessons/120894
+
+function solution(numbers) {
+  const filter = {
+      "zero" : 0,
+      "one" : 1,
+      "two" : 2,
+      "three" : 3,
+      "four" : 4,
+      "five" : 5,
+      "six" : 6,
+      "seven" : 7,
+      "eight" : 8,
+      "nine" : 9
+  }
+  for (let c of Object.keys(filter)){
+      const reg = new RegExp(`${c}`, "g");
+      numbers = numbers.replace(reg, filter[c]);
+  }
+  return parseInt(numbers);
+}
