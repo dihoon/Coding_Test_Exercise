2d1b9bfde15d823ddf1825422b680a31e1656c01
dihoon <dihoon955@gmail.com>
2023-04-07 21:27:38 +0900

    평행

---
+++ Javascript/Programmers/120875.js
@@ -0,0 +1,6 @@
+// 평행
+// https://school.programmers.co.kr/learn/courses/30/lessons/120875
+
+function solution(dots) {
+  return ((dots[0][1]-dots[1][1])/(dots[0][0]-dots[1][0]) === (dots[2][1]-dots[3][1])/(dots[2][0]-dots[3][0])) || ((dots[0][1]-dots[2][1])/(dots[0][0]-dots[2][0]) === (dots[1][1]-dots[3][1])/(dots[1][0]-dots[3][0])) || ((dots[0][1]-dots[3][1])/(dots[0][0]-dots[3][0]) === (dots[1][1]-dots[2][1])/(dots[1][0]-dots[2][0])) ? 1 : 0;
+}
