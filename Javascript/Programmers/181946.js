c99c03d3cfaf48b91af61b4a2b0b8cc690f422f5
dihoon <dihoon955@gmail.com>
2023-05-05 18:57:48 +0900

    문자열 붙여서 출력하기

---
+++ Javascript/Programmers/181946.js
@@ -0,0 +1,16 @@
+// 문자열 붙여서 출력하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181946
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    console.log(input[0]+input[1]);
+});
